c62793b6530415e2788a29134278b406b4b963cb
Viktor Orlov <v706301@gmail.com>
2014-03-28 12:02:37 +0200

    SOMIR-NG - INITIAL COMMIT

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ang1App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
